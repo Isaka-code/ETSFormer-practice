b925c6e88030c9cd1b5d6a2f9c88cf54670af1bd
Isaka Tsuyoshi <tsuyoshi1015osaka@gmail.com>
2022-12-17 20:58:46 +0900

    [update] 学習データの件数をスライダーで設定できるようにした

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -22,26 +20,35 @@ model = setup_ETSformer()
 
 # streamlitにメッセージ表示
 st.title('ETSformerで時系列予測')
-st.write("AirPassengers.csv（12年×12ヶ月）を使用します。11年を学習に使い、残り1年を予測します。")
-st.write("AirPassengers.csvは、下記の外部サイトよりダウンロードが可能です。")
-st.write("https://www.analyticsvidhya.com/wp-content/uploads/2016/02/AirPassengers.csv")
+st.markdown("[AirPassengers.csv](https://www.analyticsvidhya.com/wp-content/uploads/2016/02/AirPassengers.csv)（12年×12ヶ月=144件）を使用します。")
+st.write("11年を学習に使い、残り1年を予測します。")
 
 # 時系列データ読み込み
 df = pd.read_csv("./data/AirPassengers.csv", delimiter=",")
 # 144 = 12年×12ヶ月。11年を学習に使い、残り1年を予測します。 
-train_df, val_df = train_test_split(df, train_size=132/144, shuffle=False)
+total_num = len(df) # 144
+train_num = st.slider(label='学習データの件数',
+                    min_value=0,
+                    max_value=144,
+                    value=132,
+                    step=2 # 2ヶ月刻み
+                    )
+st.write(f'Selected: {train_num}')
+val_num = total_num - train_num # 12
+train_df, val_df = df.loc[:train_num-1, :], df.loc[train_num:, :]
+print(len(train_df))
+print(len(val_df))
 train, val = torch.tensor(train_df["#Passengers"].values, dtype=torch.float), torch.tensor(val_df["#Passengers"].values, dtype=torch.float)
 train, val = torch.reshape(train, (1, -1, 1)), torch.reshape(val, (1, -1, 1))
 
-num_steps_forecast = len(val_df)
-pred = model(train, num_steps_forecast = num_steps_forecast) # (1, 32, 4) - (batch, num steps forecast, num time features)
+pred = model(train, num_steps_forecast = val_num) # (1, 32, 4) - (batch, num steps forecast, num time features)
 pred = torch.reshape(pred, (-1,)).detach().numpy()
 
 # 図の描画
 fig, ax = plt.subplots()
 
-ax.plot(list(range(0,len(train_df))), train_df["#Passengers"].values, label="past")
-ax.plot(list(range(len(train_df),len(df))), pred, label="prediction") #予測値リストのサイズに合うようにrangeの区間を設定。
+ax.plot(list(range(0,train_num)), train_df["#Passengers"].values, label="past")
+ax.plot(list(range(train_num,total_num)), pred, label="prediction") #予測値リストのサイズに合うようにrangeの区間を設定。
 plt.grid()
 
 plt.legend()
